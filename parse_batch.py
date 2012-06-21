aabf8b9014d1a8da4d27eae49b23be471bd16ce5
vortic <vortic@berkeley.edu>
2012-06-21 16:58:57 -0700

    added parse_batch.py
    to parse multiple experiments

---
+++ parse_batch.py
@@ -0,0 +1,59 @@
+import os
+import argparse
+import src.datageneration.util as util
+import parse_output
+
+def generate_html(dir):
+    filenames = os.listdir(dir)
+    results = []
+    files_found = 0
+    for filename in filenames:
+        if "experiment" in filename and filename[-5:] == ".json":
+            files_found += 1
+            results.append(parse_output.parse(dir + '/' + filename, 
+                                              external=False))
+        else:
+            continue
+    print "processed", files_found, "files"
+    with open('data/test.html', 'w') as f:
+        f.write('<table border="1">\n')
+        f.write('\t<th colspan="20">Experiments</th>\n')
+        names = True
+        for result in results:
+            f.write('\t<tr>\n')
+            scores = []
+            for algorithm in result[2]:
+                if names and len(result[2].keys()) == 8:
+                    f.write('\t\t<td>' + algorithm + '</td>\n')
+                elif not names and len(result[2].keys()) == 10:
+                    f.write('\t\t<td>' + algorithm + '</td>\n')
+                scores.append(result[2][algorithm]['Predicted_Mean'])
+            f.write('\t</tr>\n')
+            names = False
+            f.write('\t<tr>\n')
+            for score in scores:
+                f.write('\t\t<td>' + str(round(score, 2)) + '</td>\n')
+            f.write('\t</tr>\n')
+#            for name in result[0]:
+#                f.write('\t\t<td>' + name + '</td>\n')
+#            f.write('\t</tr>\n')
+#            f.write('\t<tr>\n')
+#            for score in result[1]:
+#                f.write('\t\t<td>' + str(score) + '</td>\n')
+#            f.write('\t</tr>\n')
+        f.write('</table>')
+    return results
+
+def main():
+    parser = argparse.ArgumentParser(description="reads all json files in a \
+    directory and writes an html table displaying results")
+    parser.add_argument('f', metavar='directory', action="store", 
+                        help="directory containing json files to be read")
+    
+    args = parser.parse_args()
+    print "reading experiment files from directory:", args.f
+    
+    return generate_html(args.f)
+
+if __name__ == '__main__':
+    results = main()
--- parse_output.py
+++ parse_output.py
@@ -4,7 +4,20 @@ import src.datageneration.util as util
 
 TEST = 'tests'
 
-def parse(filename, table=False, show=False):
+def parse(filename, table=False, show=False, external=True):
+    """
+    Assumes json file is of the form: 
+    dict with one entry 'tests', which is a one-element list of dicts.
+    The inner dict has experiment names as the keys and dicts with various
+    performance metrics as values
+    
+    Args:
+        external:
+            flag for whether this function is being called from the command 
+            line (set this to False if you want to call it from another python
+            script)
+    
+    """
     with open(filename, 'r') as f:
         data = json.load(f)
     names = []
@@ -13,23 +26,25 @@ def parse(filename, table=False, show=False):
         for key in sorted(test.keys(), cmp=(lambda x,y: cmp(test[x], test[y]))):
             names.append(str(key))
             scores.append(test[key]['Predicted_Mean'])
-    util.plot_dist(scores, labels=names)
-    if table:
-        """so hacky I don't even want to talk about it"""
-        data_info = make_table()
-        sig_words, sum_squares, num_docs, avg_words = data_info
-        util.plot(0, label='sig_words: ' + str(round(sig_words, 2)))
-        util.plot(0, label='sum_squares: ' + str(round(sum_squares, 2)))
-        util.plot(0, label='num_docs: ' + str(num_docs))
-        util.plot(0, label='avg_words: ' + str(round(avg_words, 2)))
-        util.legend(loc='best')
-        
-    if show:
-        util.show()
-    else:
-        util.savefig(filename + '.pdf')
     
-    return data
+    if external:
+        util.plot_dist(scores, labels=names)
+        if table:
+            """so hacky I don't even want to talk about it"""
+            data_info = make_table()
+            sig_words, sum_squares, num_docs, avg_words = data_info
+            util.plot(0, label='sig_words: ' + str(round(sig_words, 2)))
+            util.plot(0, label='sum_squares: ' + str(round(sum_squares, 2)))
+            util.plot(0, label='num_docs: ' + str(int(num_docs)))
+            util.plot(0, label='avg_words: ' + str(round(avg_words, 2)))
+            util.legend(loc='best')
+           
+        if show:
+            util.show()
+        else:
+            util.savefig(filename + '.pdf')
+    
+    return names, scores, data[TEST][0]
 
 def make_table():
     with open('src/datageneration/output/documents_other-out', 'r') as f:
